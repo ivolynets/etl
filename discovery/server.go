42fc0ea7e0df1ef4d7d0621175e5932a9f219f8a
bj2i <igor.volynets@nordstrom.com>
2020-05-29 15:22:14 -0700

    fixed how the server is stopping, added logging.

--- discovery/server.go
+++ discovery/server.go
@@ -2,10 +2,16 @@ package discovery
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"sync"
 	"time"
 )
+
+// TODO: move logging capabilities to separate package
+var infoLog *log.Logger = log.New(os.Stdout, "discovery: ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
+var errorLog *log.Logger = log.New(os.Stderr, "discovery: ", log.LstdFlags|log.Lmicroseconds|log.Lmsgprefix)
 
 // Server serves requests from other nodes in the grid.
 type Server struct {
@@ -32,20 +38,39 @@ func NewServer() *Server {
 // this case don't forget to defer Stop method call.
 func (s *Server) Start(addr string) error {
 
-	err := s.init(addr)
-	if err != nil {
+	// initialize the server
+
+	infoLog.Println("starting server...")
+	if err := s.init(addr); err != nil {
+		errorLog.Println(err)
 		return err
 	}
+	infoLog.Println("server started")
+
+	// listen to incoming requests
 
 	buf := make([]byte, 1024)
 
 	for {
-		err := s.read(buf)
-		if err != nil {
+		if req, ok, err := s.read(buf); !ok {
+			break // server has been stopped
+		} else if err == nil {
+
+			n := 0
+			if req != nil {
+				n = len(req.data)
+				s.buf <- req
+			}
+
+			infoLog.Printf("read_bytes = %d\n", n)
+
+		} else {
+			errorLog.Println(err)
 			return err
 		}
 	}
 
+	return nil
 }
 
 // Stop shuts down the discovery server.
@@ -55,13 +80,18 @@ func (s *Server) Stop() error {
 	defer s.mux.Unlock()
 
 	if s.conn == nil {
-		return fmt.Errorf("Server is not running")
+		msg := "server is not running"
+		errorLog.Println(msg)
+		return fmt.Errorf(msg)
 	}
+
+	infoLog.Println("stopping server...")
 
 	close(s.buf)
 	s.conn.Close()
 	s.conn = nil
 
+	infoLog.Println("server is stopped")
 	return nil
 }
 
@@ -72,16 +102,17 @@ func (s *Server) init(addr string) error {
 	defer s.mux.Unlock()
 
 	if s.conn != nil {
-		return fmt.Errorf("Server is already running")
+		return fmt.Errorf("server is already running")
 	}
 
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
-	if err != nil {
+	var udpAddr *net.UDPAddr
+	var err error
+
+	if udpAddr, err = net.ResolveUDPAddr("udp", addr); err != nil {
 		return err
 	}
 
-	s.conn, err = net.ListenUDP("udp", udpAddr)
-	if err != nil {
+	if s.conn, err = net.ListenUDP("udp", udpAddr); err != nil {
 		return err
 	}
 
@@ -91,45 +122,54 @@ func (s *Server) init(addr string) error {
 	// go routine for datagrams handling
 
 	go func() {
-
 		for {
-			req, ok := <-s.buf
-			if !ok {
+			if req, ok := <-s.buf; ok {
+				infoLog.Printf("addr = %s, data = %s\n", req.addr, req.data)
+			} else {
 				// server is stopped
 				return
 			}
-
-			fmt.Printf("%s: %s\n", req.addr, req.data)
 		}
 	}()
 
 	return nil
 }
 
-// read reads the datagram and sends it to the handler.
-func (s *Server) read(buf []byte) error {
+// read reads the request from the connection. This function is timing out if
+// no bytes have been received during past second, in this case the nil
+// reference to request is returned. The second return parameter is always
+// true unless the server was stopped during the last attempt and connection
+// was closed.
+func (s *Server) read(buf []byte) (*Request, bool, error) {
 
 	var n int
 	var addr *net.UDPAddr
+	var err error
 
 	s.mux.Lock()
 	defer s.mux.Unlock()
 
+	if s.conn == nil {
+		return nil, false, nil
+	}
+
 	// TODO: make read timeout configurable
-	err := s.conn.SetReadDeadline(time.Now().Add(time.Second))
-	if err == nil {
+	if err = s.conn.SetReadDeadline(time.Now().Add(time.Second)); err == nil {
 		n, addr, err = s.conn.ReadFromUDP(buf)
 	}
 
 	if err != nil {
 		// ignore timeout error
 		if netErr, ok := err.(net.Error); !ok || !netErr.Timeout() {
-			return err
+			return nil, true, err
 		}
 	}
 
-	fmt.Printf("read: %d\n", n)
-	s.buf <- &Request{addr, string(buf[0:n])}
+	if n > 0 {
+		data := make([]byte, n)
+		copy(data, buf[0:n])
+		return &Request{addr, string(data)}, true, nil
+	}
 
-	return nil
+	return nil, true, nil
 }
--- discovery/server_test.go
+++ discovery/server_test.go
@@ -1,53 +1,69 @@
 package discovery
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"net"
-	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestBasic(t *testing.T) {
 
-	fmt.Println("Starting discovery server...")
+	var stdout bytes.Buffer
+	infoLog.SetOutput(&stdout)
+
+	var errout bytes.Buffer
+	errorLog.SetOutput(&errout)
+
 	server := NewServer()
 
-	go func() {
-
-		err := server.Start(":9999")
-
-		if err != nil {
-
-			fmt.Fprintf(os.Stderr, "Could not start the discovery server: %s\n", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("Discovery server started")
-
+	go func() error {
+		return server.Start(":9999")
 	}()
 
 	addr, err := net.ResolveUDPAddr("udp", "255.255.255.255:9999")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		t.Fatal(err)
 	}
+
+	time.Sleep(10 * time.Second)
 
 	conn, err := net.DialUDP("udp", nil, addr)
+	defer conn.Close()
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		t.Fatal(err)
 	}
 
-	for i := 0; i < 10; i++ {
-		n, err := conn.Write([]byte("Hello"))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-		}
-		fmt.Printf("write: %d\n", n)
-		time.Sleep(time.Second)
+	if _, err := conn.Write([]byte("Hello")); err == nil {
+		time.Sleep(10 * time.Second)
+	} else {
+		t.Fatal(err)
 	}
 
-	conn.Close()
 	server.Stop()
+
+	buf := bufio.NewReader(&stdout)
+	ok := false
+	for {
+		if line, _, err := buf.ReadLine(); err == nil {
+			if ok = strings.Contains(string(line), "data = Hello"); ok {
+				break
+			}
+		} else {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println(err)
+		}
+	}
+
+	if !ok {
+		t.Fatal("data has not been received by the server")
+	}
+
 }
